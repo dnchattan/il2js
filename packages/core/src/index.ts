ed3ba8fb746e14d821661247fa0bedaff6b5cf6a
dnchattan <dnchattan@gmail.com>
2021-04-30 17:34:07 -0400

    add to core exports (#13)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,11 +2,15 @@ import * as types from './NativeTypes';
 
 export * from './Address';
 export * from './BindTypeArgs';
+export * from './BufferContext';
+export * from './Dereference';
 export * from './FieldSymbols';
 export * from './Helpers';
+export * from './IOContext';
 export * from './NativeType';
 export * from './NativeTypes';
 export * from './PrimitiveTypes';
 export * from './TypeHelpers';
+export * from './Zone';
 
 export const typeExport = types;
