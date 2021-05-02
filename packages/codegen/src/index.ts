6862cb5649215cf2b8e3ef8b55b89e6d093e233d
dnchattan <dnchattan@gmail.com>
2021-05-02 10:52:52 -0400

    Add typeoutput visitor (#15)
    * add filteroutput visitor
    
    * Change files

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -3,6 +3,7 @@ import { codegen } from './Codegen';
 export * from './Codegen';
 export * from './GameAssembly';
 export * from './Targets';
+export * from './OutputFilter';
 export * from './Types';
 
 export default codegen;
